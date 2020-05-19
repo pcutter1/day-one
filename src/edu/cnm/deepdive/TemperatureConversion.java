0c108939b0d58a96630039958c8e95a0e4251a6b
Paul C <pcutter@cnm.edu>
2020-05-19 10:03:02 -0600

    Added conversion for arrays of celsius temps

--- src/edu/cnm/deepdive/TemperatureConversion.java
+++ src/edu/cnm/deepdive/TemperatureConversion.java
@@ -2,6 +2,10 @@ package edu.cnm.deepdive;
 
 public class TemperatureConversion {
 
+  private static final double FAHRENHEIT_TO_CELSIUS_SCALE = 5.0 / 9.0;
+  private static final double CELSIUS_TO_FAHRENHEIT_SCALE = 1 / FAHRENHEIT_TO_CELSIUS_SCALE;
+  private static final double SCALE_OFFSET = 32;
+
   public static void main(String[] args) {
     if (args.length == 0) {
       double tempCelsius = 100;
@@ -17,11 +21,20 @@ public class TemperatureConversion {
   }
 
   public static double convertC2F(double celsius) {
-    return celsius * 9 / 5 + 32;
+    return celsius * CELSIUS_TO_FAHRENHEIT_SCALE + SCALE_OFFSET;
   }
 
   public static double convertF2C(double fahrenheit) {
-    return (fahrenheit - 32) * 5 / 9;
+    return (fahrenheit - SCALE_OFFSET) * FAHRENHEIT_TO_CELSIUS_SCALE;
   }
 
+  public static double[] convertC2F(double[] celsiusTemperatures){
+    double[] fahrenheitTemperatures = new double[celsiusTemperatures.length];
+    for (int i = 0; i < celsiusTemperatures.length; i++) {
+      double celsius = celsiusTemperatures[i];
+      double fahrenheit = convertC2F(celsius);
+      fahrenheitTemperatures[i] = fahrenheit;
+    }
+    return fahrenheitTemperatures;
+  }
 }

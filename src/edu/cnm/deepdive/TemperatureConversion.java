03a3465aad04cbb4a01e8908dbb2379edcd21978
Paul C <pcutter@cnm.edu>
2020-05-19 15:00:53 -0600

    Overloaded conversions for arrays

--- src/edu/cnm/deepdive/TemperatureConversion.java
+++ src/edu/cnm/deepdive/TemperatureConversion.java
@@ -2,20 +2,23 @@ package edu.cnm.deepdive;
 
 public class TemperatureConversion {
 
-  private static final double FAHRENHEIT_TO_CELSIUS_SCALE = 5.0 / 9.0;
-  private static final double CELSIUS_TO_FAHRENHEIT_SCALE = 1 / FAHRENHEIT_TO_CELSIUS_SCALE;
+  private static final double CELSIUS_TO_FAHRENHEIT_SCALE = 9.0 / 5.0;
+  private static final double FAHRENHEIT_TO_CELSIUS_SCALE =  1 / CELSIUS_TO_FAHRENHEIT_SCALE;
   private static final double SCALE_OFFSET = 32;
 
   public static void main(String[] args) {
     if (args.length == 0) {
       double tempCelsius = 100;
       double tempFahrenheit = 32;
-      System.out.println("Celsius " + tempCelsius + " = " + convertC2F(tempCelsius) + " Fahrenheit");
-      System.out.println("Fahrenheit " + tempFahrenheit + " = " + convertF2C(tempFahrenheit) + " Celsius");
+      System.out
+          .println("Celsius " + tempCelsius + " = " + convertC2F(tempCelsius) + " Fahrenheit");
+      System.out.println(
+          "Fahrenheit " + tempFahrenheit + " = " + convertF2C(tempFahrenheit) + " Celsius");
     } else {
       for (String arg : args) {
         double tempCelsius = Double.parseDouble(arg);
-        System.out.println("Celsius " + tempCelsius + " = " + convertC2F(tempCelsius) + " Fahrenheit");
+        System.out
+            .println("Celsius " + tempCelsius + " = " + convertC2F(tempCelsius) + " Fahrenheit");
       }
     }
   }
@@ -28,7 +31,7 @@ public class TemperatureConversion {
     return (fahrenheit - SCALE_OFFSET) * FAHRENHEIT_TO_CELSIUS_SCALE;
   }
 
-  public static double[] convertC2F(double[] celsiusTemperatures){
+  public static double[] convertC2F(double[] celsiusTemperatures) {
     double[] fahrenheitTemperatures = new double[celsiusTemperatures.length];
     for (int i = 0; i < celsiusTemperatures.length; i++) {
       double celsius = celsiusTemperatures[i];
@@ -37,4 +40,15 @@ public class TemperatureConversion {
     }
     return fahrenheitTemperatures;
   }
+
+  public static double[] convertF2C(double[] fahrenheitTemperatures) {
+    double[] celsiusTemperatures = new double[fahrenheitTemperatures.length];
+    for (int i = 0; i < fahrenheitTemperatures.length; i++) {
+      double fahrenheit = fahrenheitTemperatures[i];
+      double celsius = convertF2C(fahrenheit);
+      celsiusTemperatures[i] = celsius;
+    }
+    return celsiusTemperatures;
+  }
+
 }
